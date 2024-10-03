d21ffb80dd97c526aa7d09867ccc0b43a463c634
Anna-Yudina <anuta-nuty@yandex.ru>
2024-10-03 23:12:46 +0700

    Добавлены клавиатуры

--- src/main/java/ru/yudina/tutorbot/service/handler/CommandHandler.java
+++ src/main/java/ru/yudina/tutorbot/service/handler/CommandHandler.java
@@ -1,29 +1,86 @@
 package ru.yudina.tutorbot.service.handler;
 
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Message;
+import ru.yudina.tutorbot.service.factory.KeyboardFactory;
 import ru.yudina.tutorbot.telegram.Bot;
 
+import java.util.List;
+
+import static ru.yudina.tutorbot.service.data.Command.*;
+
 @Service
+@AllArgsConstructor
 public class CommandHandler {
-    public BotApiMethod<?> answer(Message message, Bot bot){
+
+    private final KeyboardFactory keyboardFactory;
+
+    public BotApiMethod<?> answer(Message message, Bot bot) {
         String command = message.getText();
-        switch (command){
-            case "/start" -> {
+        switch (command) {
+            case START -> {
                 return start(message);
             }
+            case FEEDBACK -> {
+                return feedback(message);
+            }
+            case HELP -> {
+                return help(message);
+            }
+            default -> {
+                return defaultAnswer(message);
+            }
         }
-        return null;
     }
 
-    private BotApiMethod<?> start(Message message){
+    private BotApiMethod<?> start(Message message) {
         return SendMessage.builder()
                 .chatId(message.getChatId())
                 .text("""
                         Приветствую в Tutor-Bot
                         """)
+                .replyMarkup(keyboardFactory.getInlineKeyboard(
+                        List.of("Помощь", "Обратная связь"),
+                        List.of(2),
+                        List.of("help", "feedback")
+                ))
+                .build();
+    }
+
+    private BotApiMethod<?> feedback(Message message) {
+        return SendMessage.builder()
+                .chatId(message.getChatId())
+                .text("""
+                        Ссылки для обратной связи
+                        https://t.me/Anuta_Nuty
+                        """)
+                .build();
+    }
+
+    private BotApiMethod<?> help(Message message) {
+        return SendMessage.builder()
+                .chatId(message.getChatId())
+                .text("""
+                        Доступные команды:
+                        - start
+                        - feedback
+                        - help
+                                                
+                        Доступные функции:
+                        - Расписание
+                        - Домашнее задание
+                        - Контроль успеваемости
+                        """)
+                .build();
+    }
+
+    private BotApiMethod<?> defaultAnswer(Message message) {
+        return SendMessage.builder()
+                .chatId(message.getChatId())
+                .text("Неподдерживаемая команда")
                 .build();
     }
 }
